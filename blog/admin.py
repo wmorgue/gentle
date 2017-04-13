4320257299fd484771e089215e41ead9716991db
born2lose <born2lose@sigaint.com>
2017-04-13 14:04:22 +0300

    refresh admin

--- blog/admin.py
+++ blog/admin.py
@@ -4,8 +4,14 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     """Вывод в админку описание и автор, а так же поиск по описанию"""
-    list_display = ('title', 'author', 'created')
-    search_fields = ['title']
+    list_display = ['title', 'author', 'created', 'updated']
+    list_display_links = ['updated']
+    list_filter = ['created']
+    list_editable = ['title']
+    search_fields = ['title, text']
+
+    class Meta:
+        model = Post
 
 
 admin.site.register(Post, PostAdmin)
