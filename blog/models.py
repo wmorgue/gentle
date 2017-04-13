602e4b9b6c75099fdb682d0d626afecf1b31a39f
born2lose <born2lose@sigaint.com>
2017-04-13 13:24:53 +0300

    reedit models

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,13 @@
 from django.db import models
-from django.utils import timezone
 
 
 class Post(models.Model):
-    """Стандарная модель"""
+    """Стандарная модель MVC MODEL VIEW CONTROLLER"""
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=150)
     text = models.TextField()
-    created = models.DateTimeField(default=timezone.now)
-    published = models.DateTimeField(blank=True, null=True)
-
-    def publish_recent(self):
-        return self.published >= timezone.now() - datetime.timedelta(days=1)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
 
     def __str__(self):
         return self.title
