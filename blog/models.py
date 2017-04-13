09c8bd4db45dbf186500529c967a7da73a448456
born2lose <born2lose@sigaint.com>
2017-04-13 17:18:37 +0300

    edit models.py

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Post(models.Model):
@@ -11,3 +12,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'id': self.id})
