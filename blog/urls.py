0ebd995eb2184d2a68f1913ceb6955662ab0665f
born2lose <born2lose@sigaint.com>
2017-04-14 22:06:45 +0300

    new patterns

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^about/', views.about, name='about'),
     url(r'^blog/', views.index, name='index'),
-    url(r'^create/', views.form_create, name='create'),
+    url(r'^create/$', views.form_create, name='create'),
+    url(r'^(?P<id>\d+)/edit/$', views.update, name='update'),
     url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
 ]
