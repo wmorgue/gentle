3d981baa11a561f3eda9b612bad19fab9637461e
born2lose <born2lose@sigaint.com>
2017-04-16 16:01:19 +0300

    add rules and media root

--- gentle/settings.py
+++ gentle/settings.py
@@ -98,5 +98,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
--- gentle/urls.py
+++ gentle/urls.py
@@ -1,6 +1,8 @@
 """gentle URL Configuration"""
-from django.conf.urls import url, include
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -9,3 +11,6 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
